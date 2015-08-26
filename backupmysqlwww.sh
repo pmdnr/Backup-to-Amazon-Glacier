0438343ffff3417f4c138fff0647dad5bbaffe35
PMD Nagarjuna <pmdnr@nagabot.com>
2015-08-26 14:51:41 +0530

    Update backupmysqlwww.sh

--- backupmysqlwww.sh
+++ backupmysqlwww.sh
@@ -4,9 +4,9 @@ _file="yourwebsitename_all_$_now.sql"
 _file1="yourwebsitename_html_all_$_now.tar.bz2"
 _file2="yourwebsitename_db_all_$_now.tar.bz2"
 echo "Starting MySQL Database Backup to $_file..."
-mysqldump -u mySQLUser -p'yourMySQLpassword' --all-databases > "$_file"
+mysqldump -u yourMySQLUser -p'yourMySQLpassword' --all-databases > "$_file"
 tar cvfj "$_file2" "$_file"
 rm -rf "$_file"
 echo "Starting HTML Files Backup to $_file..."
-tar cvfj "$_file1" /usr/share/nginx/html
+tar cvfj "$_file1" yourFolderPathToBackup
 php backuptoamazonglacier.php >> ArchiveIDS.txt
