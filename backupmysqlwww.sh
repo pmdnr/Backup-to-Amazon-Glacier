cc9eb7c0704749af39089e5a88c54579e68f98a3
PMD Nagarjuna <pmdnr@nagabot.com>
2015-08-26 14:52:27 +0530

    Update backupmysqlwww.sh

--- backupmysqlwww.sh
+++ backupmysqlwww.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 _now=$(date +"%m_%d_%Y")
-_file="yourwebsitename_all_$_now.sql"
-_file1="yourwebsitename_html_all_$_now.tar.bz2"
-_file2="yourwebsitename_db_all_$_now.tar.bz2"
+_file="yourWebsitename_all_$_now.sql"
+_file1="yourWebsitename_html_all_$_now.tar.bz2"
+_file2="yourWebsitename_db_all_$_now.tar.bz2"
 echo "Starting MySQL Database Backup to $_file..."
 mysqldump -u yourMySQLUser -p'yourMySQLpassword' --all-databases > "$_file"
 tar cvfj "$_file2" "$_file"
